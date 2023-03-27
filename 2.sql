ba040d055bd1b60798ee474a6298db9d85be4b16
splean101 <101splean@gmail.com>
2023-03-27 16:58:13 +0300

    2

---
+++ 2.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(Price) FROM products WHERE category='laptops';
+SELECT COUNT(*) FROM products WHERE category='smartphones';
+SELECT DISTINCT brand FROM products WHERE category='laptops';
+SELECT DISTINCT brand FROM products WHERE category='smartphones';
